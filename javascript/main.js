d49cfff2ee20b7fc209286e014497e143807ff70
slyty7 <tyler.j.west.7@gmail.com>
2017-09-27 11:10:25 -0700

    add functionality to calculator

--- javascript/main.js
+++ javascript/main.js
@@ -1,54 +1,43 @@
 $(document).ready(function(){
-
 	var result = 0;
 	var prevEntry = 0;
 	var currentEntry = "0";
 	updateScreen(result);
 
 
+	//function that sees what button is pressed and then changes the screen to reflect that
 	$("button").click(function(){
-		const buttonPressed = $(this).html();
-		console.log(buttonPressed);
+		var buttonPressed = $(this).html();
 
 		if(buttonPressed === "C"){
 			result = 0;
 			currentEntry = "0";
 		}else if (buttonPressed === "CE") {
       		currentEntry = '0';
+    	}else if (buttonPressed === "BK") {
+    		currentEntry = currentEntry.substring(0, currentEntry.length - 1);
+    	}else if (buttonPressed === "+/-") {
+      		currentEntry *= -1;
+    	}else if (buttonPressed === ".") {
+      		currentEntry += ".";
+    	}else if (isNumber(buttonPressed)) {
+      		if (currentEntry === '0'){
+      			currentEntry = buttonPressed;
+      		}else {
+      			currentEntry = currentEntry + buttonPressed;
+      		}
+    	}else if (isOperator(buttonPressed)) {
+      		prevEntry = parseFloat(currentEntry);
+      		operation = buttonPressed;
+      		currentEntry = '';
+    	}else if (buttonPressed === '=') {
+      		currentEntry = operate(prevEntry, currentEntry, operation);
+      		operation = null;
     	}
 
 
     	updateScreen(currentEntry);
 	});
-
-
-
-
-
-
-
-/*	var input = $("#user-input");
-
-
-	$("#equals-button").click(function(){
-		console.log(input.val());
-	});
-
-	$(".number-button").click(function(){
-		var buttonVal = $(this).text();
-		input.val("" + input.val() + "" + buttonVal);
-	});
-
-	$(".clear-button").click(function(){
-		input.val("");
-	});
-
-	function add(x,y){
-		return x + y;
-	};
-
-	console.log(add(1,2));*/
-
 
 });
 
@@ -57,4 +46,23 @@ function updateScreen(display){
 	//change value of the screen to a string
 	var display = display.toString();
 	$(".screen").val(display);
+}
+
+
+function isNumber(val){
+	return !isNaN(val);
+}
+
+function isOperator(value) {
+ 	return value === '/' || value === '*' || value === '+' || value === '-';
+}
+
+function operate(x, y, operation){
+	x = parseFloat(x);
+	y = parseFloat(y);
+	
+	if (operation === '+') return x + y;
+ 	if (operation === '-') return x - y;
+ 	if (operation === '*') return x * y;
+ 	if (operation === '/') return x / y;
 }
